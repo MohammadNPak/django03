0f5c89f8926e3e368fb63589cc7a18ac71a47fd8
Teacher <teacher@engmft.com>
2022-03-11 19:12:52 +0330

    s01 final commit

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,"blog/index.html",{})
+
+def posts(request):
+    return render(request,"blog/posts.html",{})
--- devconnector/urls.py
+++ devconnector/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from blog.views import index
+from blog import urls as blog_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("blog/",include(blog_urls)),
     path('',index)
 ]
